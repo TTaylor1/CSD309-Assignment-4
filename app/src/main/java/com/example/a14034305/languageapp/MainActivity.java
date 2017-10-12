e14b13aedd354822515d577a594f496bc9117500
IASGROUP\14034305 <shorute69@gmail.com>
2017-10-11 21:01:33 -0400

    Assignment #4

---
+++ app/src/main/java/com/example/a14034305/languageapp/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.a14034305.languageapp;
+
+import android.app.ActionBar;
+import android.os.Build;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    private void setUpActionBar() {
+        // Make sure we're running on Honeycomb or higher to use ActionBar APIs
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+            ActionBar actionBar = getActionBar();
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+    }
+}
